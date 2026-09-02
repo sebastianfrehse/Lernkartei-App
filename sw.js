86d67a3f93ce13984868c5accff69a774d49841c
sebastianfrehse <sebastianfrehse@gmail.com>
2026-09-02 10:25:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Minimaler Service Worker, nur damit die App als "installierbar" erkannt wird.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', () => {});
